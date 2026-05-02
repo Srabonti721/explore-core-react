767ab5fea5b9326f2c7f34c00c1bf65c5bde067f
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-05-02 22:18:38 +0600

    ore option to conditional rendering

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -4,14 +4,52 @@
 //     )
 // }
 
-export default function ToDo ({name, isDown, time = 0}){
-if(isDown === true){
-    return <li className="shared">Done : {name} duration: {time}</li>
+// conditional rendering - 1: if else
+// export default function ToDo ({name, isDown, time = 0}){
+// if(isDown === true){
+//     return <li className="shared">Done : {name} duration: {time}</li>
+// }
+// else{
+//     return <li className="student"> Not Done : {name} duration : {time}</li>
+// }
+// }
+
+// conditional rendering - 2: if 
+// export default function ToDo ({name, isDown, time}){
+//     if(isDown === true){
+//         return <li className="shared">Done : {name} duration: {time}</li>
+//     }
+//     return <li className="student"> not down : {name} duration : {time}</li>
+// }
+
+// conditional rendering - 3: ternary operator
+// export default function ToDo ({name, isDown, time}){
+//     return isDown === true? <li className="shared">Done : {name} duration: {time}</li>: <li className="student"> not down : {name} duration : {time}</li>
+// }
+
+// conditional rendering - 4 : &&
+// export default function ToDo ({name, isDown, time}){
+//     return isDown && <li className="shared">Done : {name} duration: {time}</li>
+// }
+
+// conditional rendering - 5: ||
+// export default function ToDo ({name, isDown, time}){
+// return isDown || <li className=" student">not down : {name}, duration : {time}</li>
+// }
+
+// conditional rendering - 6: variable
+export default function ToDo ({name, isDown , time}){
+    // if(isDown=== true){
+    //     return null;
+    // }
+    // return <li className=" student">not down : {name}, duration : {time}</li>
+
+    let listItem ;
+    if(isDown){
+        listItem = <li className="student">done : {name}, duration : {time}</li>
+    }
+    else{
+        listItem = <li className="student">not down : {name}, duration : {time}</li>
+    }
+    return listItem;
 }
-else{
-    return <li className="student"> Not Done : {name} duration : {time}</li>
-}
-} 
-
-
-
